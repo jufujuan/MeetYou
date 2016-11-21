2cae1e2f24ca0117849cd02ef4da4b2e7248e653
鞠福娟 <2376562567@qq.com>
2016-11-21 17:05:42 +0800

    基本的分包

--- app/src/main/java/com/zrj/dllo/meetyou/controller/activity/MainActivity.java
+++ app/src/main/java/com/zrj/dllo/meetyou/controller/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.zrj.dllo.meetyou;
+package com.zrj.dllo.meetyou.controller.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.zrj.dllo.meetyou.R;
 
 public class MainActivity extends AppCompatActivity {
 

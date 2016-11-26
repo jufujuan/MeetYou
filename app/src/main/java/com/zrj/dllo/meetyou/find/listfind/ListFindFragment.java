89af55ec04be3a824a89159b6f9b5b38dc455ffc
鞠福娟 <2376562567@qq.com>
2016-11-26 14:02:33 +0800

    重新创建FindlistFragment

---
+++ app/src/main/java/com/zrj/dllo/meetyou/find/listfind/ListFindFragment.java
@@ -0,0 +1,26 @@
+package com.zrj.dllo.meetyou.find.listfind;
+
+import com.zrj.dllo.meetyou.R;
+import com.zrj.dllo.meetyou.base.AbsBaseFragment;
+
+/**
+ * 这是 鞠福娟 创建的哟~
+ * on 16/11/26.
+ */
+
+public class ListFindFragment extends AbsBaseFragment{
+    @Override
+    protected int getLayout() {
+        return R.layout.fra_list_find;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}

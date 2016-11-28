dd24adebd14906aff5bd85ac9f169e22799192d8
鞠福娟 <2376562567@qq.com>
2016-11-28 16:11:04 +0800

    bug修改

--- app/src/main/java/com/zrj/dllo/meetyou/find/mainfind/FindFragment.java
+++ app/src/main/java/com/zrj/dllo/meetyou/find/mainfind/FindFragment.java
@@ -9,6 +9,7 @@ import android.view.animation.AccelerateInterpolator;
 import android.view.animation.ScaleAnimation;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zrj.dllo.meetyou.MainActivity;
 import com.zrj.dllo.meetyou.R;
@@ -115,7 +116,6 @@ public class FindFragment extends AbsBaseFragment implements FindContract.View, 
         initLocation();
         showClickAnim();
         mLocationClient.start();
-        goTo(context, MainActivity.class,Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
     }
 
     private void initLocation(){
@@ -140,6 +140,86 @@ public class FindFragment extends AbsBaseFragment implements FindContract.View, 
 
         @Override
         public void onReceiveLocation(BDLocation location) {
+            switch (location.getLocType()){
+                case 61:
+                    Toast.makeText(context, "GPS定位成功", Toast.LENGTH_SHORT).show();
+                    goTo(context, MainActivity.class,Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                    break;
+                case 62:
+                    break;
+                case 63:
+                    break;
+                case 64:
+                    break;
+                case 65:
+                    break;
+                case 66:
+                    Toast.makeText(context, "离线定位结果", Toast.LENGTH_SHORT).show();
+                    break;
+                case 67:
+                    break;
+                case 68:
+                    break;
+                case 161:
+                    Toast.makeText(context, "网络定位成功", Toast.LENGTH_SHORT).show();
+                    goTo(context,MainActivity.class,Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                    break;
+                case 162:
+                    break;
+                case 167:
+                    break;
+                case 502:
+                    break;
+                case 505:
+                    break;
+                case 601:
+                    break;
+                case 602:
+                    break;
+                default:
+                    break;
+            }
+
+//            if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+//                sb.append("\nspeed : ");
+//                sb.append(location.getSpeed());// 单位：公里每小时
+//                sb.append("\nsatellite : ");
+//                sb.append(location.getSatelliteNumber());
+//                sb.append("\nheight : ");
+//                sb.append(location.getAltitude());// 单位：米
+//                sb.append("\ndirection : ");
+//                sb.append(location.getDirection());// 单位度
+//                sb.append("\naddr : ");
+//                sb.append(location.getAddrStr());
+//                sb.append("\ndescribe : ");
+//                sb.append("gps定位成功");
+//            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+//                sb.append("\naddr : ");
+//                sb.append(location.getAddrStr());
+//                //运营商信息
+//                sb.append("\noperationers : ");
+//                sb.append(location.getOperators());
+//                sb.append("\ndescribe : ");
+//                sb.append("网络定位成功");
+//            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+//                sb.append("\ndescribe : ");
+//                sb.append("离线定位成功，离线定位结果也是有效的");
+//            } else if (location.getLocType() == BDLocation.TypeServerError) {
+//                sb.append("\ndescribe : ");
+//                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+//            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+//                sb.append("\ndescribe : ");
+//                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+//            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+//                sb.append("\ndescribe : ");
+//                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+//            }
+
+
+
+
+
+
             //Receive Location
             StringBuffer sb = new StringBuffer(256);
             sb.append("time : ");

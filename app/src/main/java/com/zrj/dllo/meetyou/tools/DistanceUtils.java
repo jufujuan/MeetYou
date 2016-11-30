25a2ac57f9d3bdd4bf6f3d6cc03e5d2e571f1f76
鞠福娟 <2376562567@qq.com>
2016-11-30 15:52:31 +0800

    将登陆和显示信息合好,(完成搜索附近的人的功能,代码还有待整理)

--- app/src/main/java/com/zrj/dllo/meetyou/tools/DistanceUtils.java
+++ app/src/main/java/com/zrj/dllo/meetyou/tools/DistanceUtils.java
@@ -7,73 +7,26 @@ package com.zrj.dllo.meetyou.tools;
  */
 
 public class DistanceUtils {
-    private double loOne = 0;
-    private double loTwo = 0;
-    private double laOne = 0;
-    private double laTwo = 0;
-    private static final double EARTH_RADIUS = 6378137.0;
-
-    public void setLoOne(double loOne) {
-        this.loOne = loOne;
-    }
-
-    public void setLoTwo(double loTwo) {
-        this.loTwo = loTwo;
-    }
-
-    public void setLaOne(double laOne) {
-        this.laOne = laOne;
-    }
-
-    public void setLaTwo(double laTwo) {
-        this.laTwo = laTwo;
-    }
-
-    /**
-     * 设置第一个位置的经纬度
-     *
-     * @param loOne 经度
-     * @param laOne 纬度
-     */
-    public void setOne(double loOne, double laOne) {
-        this.loOne = loOne;
-        this.laOne = laOne;
-    }
-
-    /**
-     * 设置第二位置的经纬度
-     *
-     * @param loTwo 经度
-     * @param laTwo 纬度
-     */
-    public void setTwo(double loTwo, double laTwo) {
-        this.loTwo = loTwo;
-        this.laTwo = laTwo;
-    }
-
     /**
      * 得到经纬度计算的结果
      *
-     * @param longitude1 第一个位置的经度
-     * @param latitude1  第一个位置的纬度
-     * @param longitude2 第二个位置的经度
-     * @param latitude2  第二个位置的纬度
-     * @return
+     * @param lat_a 第一个位置的经度
+     * @param lng_a  第一个位置的纬度
+     * @param lat_b 第二个位置的经度
+     * @param lng_b  第二个位置的纬度
+     * @return 距离(单位米)
      */
-    public double getDistance(double longitude1, double latitude1, double longitude2, double latitude2) {
-        double Lat1 = rad(latitude1);
-        double Lat2 = rad(latitude2);
-        double a = Lat1 - Lat2;
-        double b = rad(longitude1) - rad(longitude2);
-        double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)
-                + Math.cos(Lat1) * Math.cos(Lat2)
-                * Math.pow(Math.sin(b / 2), 2)));
-        s = s * EARTH_RADIUS;
-        s = Math.round(s * 10000) / 10000;
-        return s;
+    public static double getDistance(double lng_a,double lat_a,double lng_b,double lat_b) {
+        double pk = 180 / 3.14169;
+        double a1 = lat_a / pk;
+        double a2 = lng_a / pk;
+        double b1 = lat_b / pk;
+        double b2 = lng_b / pk;
+        double t1 = Math.cos(a1) * Math.cos(a2) * Math.cos(b1) * Math.cos(b2);
+        double t2 = Math.cos(a1) * Math.sin(a2) * Math.cos(b1) * Math.sin(b2);
+        double t3 = Math.sin(a1) * Math.sin(b1);
+        double tt = Math.acos(t1 + t2 + t3);
+        return 6366000 * tt;
     }
 
-    private double rad(double d) {
-        return d * Math.PI / 180.0;
-    }
 }
